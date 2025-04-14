14b80b405ead76269ea5f6d648e82cccee2d3fa8
Marina Franco Borges <17460083+fbmarina@users.noreply.github.com>
2025-04-14 17:04:51 -0300

    fix: background color

--- src/android/ColorHelper.kt
+++ src/android/ColorHelper.kt
@@ -2,7 +2,7 @@ package de.dustplanet.cordova.lottie
 
 import androidx.annotation.ColorInt
 import androidx.annotation.Size
-import java.lang.Long.parseLong
+import androidx.core.graphics.toColorInt
 
 class ColorHelper {
     companion object {
@@ -14,22 +14,10 @@ class ColorHelper {
 fun ColorHelper.Companion.parseColor(
     @Size(min = 1) colorString: String,
 ): Int {
-    var xColorString =
-        when {
-            colorString[0] == '#' -> colorString.substring(1)
-            else -> colorString
-        }
-
-    // Reverse alpha value
-    if (xColorString.length == 8) {
-        xColorString = xColorString.substring(6, 8) + xColorString.substring(0, 6)
+    return try {
+      colorString.toColorInt()
+    } catch (e: IllegalArgumentException) {
+      // fallback para branco
+      "#ffffff".toColorInt()
     }
-
-    var color = parseLong(xColorString, 16)
-
-    when {
-        xColorString.length == 6 -> color = color or -0x1000000
-        xColorString.length != 8 -> throw IllegalArgumentException("Unknown color")
-    }
-    return color.toInt()
 }
